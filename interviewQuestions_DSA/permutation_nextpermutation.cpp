c24527039bdb4b0a93df99c94ba0f3210568ff10
jranjan01 <9114338163gudu@gmail.com>
2026-02-07 18:18:33 +0530

    permutation

--- interviewQuestions_DSA/permutation_nextpermutation.cpp
+++ interviewQuestions_DSA/permutation_nextpermutation.cpp
@@ -69,13 +69,13 @@ int main(){
 
     // ---------------------------
 
-    // find all the Permulation
+    // find all the Permutation
     vector<int> nums = {1,2,3,4}; 
     vector<vector<int>> ans;
     int index = 0;
 
     permutation(nums, ans, index);
-    
+
     for(int i = 0; i < ans.size(); i++){
         for(int j = 0; j < ans[i].size() ; j++){
             cout << ans[i][j] << " ";
